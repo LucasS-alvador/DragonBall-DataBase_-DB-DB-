3100d6e0703823946f9dedaf21e5bb0a53341673
Antônio Henrique Rohling Froehner <rf.antoniohenrique@gmail.com>
2025-10-01 15:23:53 -0300

    Fix app.py and all src imports through bat file
    activate.bat file in .venv/Scripts was modified to initialize the cmd with "set PYTHONPATH=%VIRTUAL_ENV%\\..\\DBDBmain;%PYTHONPATH%", for the patch to work you need to configure the cmd as default

---
+++ DBDBmain/src/app.py
@@ -0,0 +1,8 @@
+from src.config import *
+from src.route.routes import *
+
+print("Application started successfully. (reached app.py)")
+
+# To run the app
+if __name__ == '__main__':
+    app.run(debug=True)
