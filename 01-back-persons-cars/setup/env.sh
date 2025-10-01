3dcaf0c89adfdcc56c9e89365e83280b205abe80
Antônio Henrique Rohling Froehner <rf.antoniohenrique@gmail.com>
2025-10-01 15:25:47 -0300

    Delete 01-back-persons-cars directory (copy)

--- 01-back-persons-cars/setup/env.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=/home/friend/02-gitlab/prog25/web/python/flask/01-back-persons-cars:$PYTHONPATH
-export MY_DB="SQLITE"
--- 01-back-persons-cars/src/app.py
+++
@@ -1,4 +0,0 @@
-from src.config import *
-from src.route.routes import *
-
-print("Application started successfully. (reached app.py)")
