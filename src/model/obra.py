f04658a09711b62d62a8f292b34bce1e601689b8
Antônio Henrique Rohling Froehner <rf.antoniohenrique@gmail.com>
2025-10-07 11:40:46 -0300

    bing chiling
    progresso na programação das classes e pytest

---
+++ src/model/obra.py
@@ -0,0 +1,15 @@
+from src.config import *
+
+class Obra(db.Model):
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(64), nullable=False)
+    dtIni: Mapped[date] = mapped_column(Date, nullable=False)
+    dtFim: Mapped[date] = mapped_column(Date, nullable=False)
+    imagem: Mapped[str] = mapped_column(String(250))
+
+    sagas: Mapped[List["Saga"]] = db.relationship(
+        "Saga", 
+        back_populates="obra", 
+        cascade="all, delete-orphan", 
+        passive_deletes=True)
