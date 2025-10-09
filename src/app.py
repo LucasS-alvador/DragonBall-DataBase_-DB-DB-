fd30cb742fc0e2e413f07dff4a81eac4fd0c310e
Antônio Henrique Rohling Froehner <rf.antoniohenrique@gmail.com>
2025-10-09 17:18:36 -0300

    Delete src directory
    outdated copy 2

--- src/app.py
+++
@@ -1,8 +0,0 @@
-from src.config import *
-from src.route.routes import *
-
-print("Application started successfully. (reached app.py)")
-
-# To run the app
-if __name__ == '__main__':
-    app.run(debug=True)
