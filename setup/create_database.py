5eb7bd45b683069dacccf829b0e655102d1ed78d
Antônio Henrique Rohling Froehner <rf.antoniohenrique@gmail.com>
2025-10-09 17:17:44 -0300

    Delete setup directory
    ts is kinda messy now

--- setup/create_database.py
+++
@@ -1,9 +0,0 @@
-from src.config import *
-from src.model.car import *
-from src.model.person import *
-
-# Create the tables in the database
-with app.app_context():
-    db.create_all()
-
-print("Database created")
