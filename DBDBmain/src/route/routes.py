c8a72434b3ac45f12470ab4c404ca4d3bb5862ef
LucasS-alvador <lucassalvador948@gmail.com>
2025-10-13 15:38:11 -0300

    Adição do frontend limpo, e .gitignore

--- DBDBmain/src/route/routes.py
+++ DBDBmain/src/route/routes.py
@@ -1,18 +1,31 @@
 from src.config import *
 from src.service.common_service import *
-from src.model.old.person import *
-from src.model.old.car import *
+from src.model.obra import *
+from src.model.saga import *
+from src.model.raca import *
+from src.model.transformacao import *
+from src.model.personagembase import *
+from src.model.personagemsaga import *
 from src.utils import *
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Obra: (Nome, data_ini, data_fin, imagem)
+# Saga: (Nome, desc, ep_ini, ep_fin, Obra_id, imagem)
+
+# Raça: (Nome, cor, desc, poderBase, imagem)
+# Transformação: (Nome, cor, poder, especial, efeitoCol, imagem, tempo_lim)
+# PersonagemBase: (Nome, Raça_id, data_nasc, sexo,  imagem)
+# PersonagemSaga: (PersonagemBase_id, Saga_id, poder, Trans_id[], imagem)
+
+
 # default route
 @app.route('/')
 def index():
-    return "Welcome to the API. Use /persons and /cars endpoints."
+    return "Welcome to the API. Use endpoints."
 
-# --- POST's (creation) ---
+# ---------------------------------------------------------------- POST's (creation) ----------------------------------------------------------------
 
 # generic object creation: auxiliar function
 def create_simple_object(mclass, data):
@@ -26,20 +39,61 @@ def create_simple_object(mclass, data):
         print(f"Error during object creation: {ex}")
         return {"result":"error", "details":f"error during object creation: {ex}"}
     
-@app.route('/persons', methods=['POST'])
-def create_person():
-    data = request.json                         # get the data
-    answer = create_simple_object(Person, data)   # try to create the object
-    return jsonify(answer), 201 if answer["result"] == "ok" else 500 # return created or internal error
+# @app.route('/persons', methods=['POST'])
+# def create_person():
+#     data = request.json                         # get the data
+#     answer = create_simple_object(Person, data) # try to create the object
+#     return jsonify(answer), 201 if answer["result"] == "ok" else 500 # return created or internal error
 
+# @app.route('/cars', methods=['POST'])
+# def create_cars():
+#     data = request.json                        # get the data
+#     answer = create_simple_object(Car, data)   # try to create the object
+#     return jsonify(answer), 201 if answer["result"] == "ok" else 500 # return created or internal error
 
-@app.route('/cars', methods=['POST'])
-def create_cars():
-    data = request.json                         # get the data
-    answer = create_simple_object(Car, data)   # try to create the object
-    return jsonify(answer), 201 if answer["result"] == "ok" else 500 # return created or internal error
-        
-# --- GET's LIST ---
+@app.route('/obra', methods=['POST'])
+def create_obra():
+    data = request.json
+    answer = create_simple_object(Obra, data)
+    return jsonify(answer), 201 if answer["result"] == "ok" else 500
+
+@app.route('/saga', methods=['POST'])
+def create_saga():
+    data = request.json
+    answer = create_simple_object(Saga, data)
+    return jsonify(answer), 201 if answer["result"] == "ok" else 500
+
+@app.route('/raca', methods=['POST'])
+def create_raca():
+    data = request.json
+    answer = create_simple_object(Raca, data)
+    return jsonify(answer), 201 if answer["result"] == "ok" else 500
+
+@app.route('/transformacao', methods=['POST'])
+def create_transformacao():
+    data = request.json
+    answer = create_simple_object(Transformacao, data)
+    return jsonify(answer), 201 if answer["result"] == "ok" else 500
+
+@app.route('/personagembase', methods=['POST'])
+def create_personagembase():
+    data = request.json
+    answer = create_simple_object(PersonagemBase, data)
+    return jsonify(answer), 201 if answer["result"] == "ok" else 500
+
+@app.route('/personagemsaga', methods=['POST'])
+def create_personagemsaga():
+    data = request.json
+    answer = create_simple_object(PersonagemSaga, data)
+    return jsonify(answer), 201 if answer["result"] == "ok" else 500
+
+# @app.route('/example', methods=['POST'])
+# def create_example():
+#     data = request.json
+#     answer = create_simple_object(Example, data)
+#     return jsonify(answer), 201 if answer["result"] == "ok" else 500
+
+# ---------------------------------------------------------------- GET's LIST ----------------------------------------------------------------
 
 # auxiliar function
 def get_objects_helper(mclass):
@@ -53,17 +107,52 @@ def get_objects_helper(mclass):
         print(f"Error during {mclass} listing: {ex}")
         return {"result": "error", "details": f"error during {mclass} listing: {ex}"}
 
-@app.route('/persons', methods=['GET'])
-def list_persons():
-    myjson = get_objects_helper(Person)
+# @app.route('/persons', methods=['GET'])
+# def list_persons():
+#     myjson = get_objects_helper(Person)
+#     return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
+
+# @app.route('/cars', methods=['GET'])
+# def list_cars():
+#     myjson = get_objects_helper(Car)
+#     return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
+
+@app.route('/obra', methods=['GET'])
+def list_obra():
+    myjson = get_objects_helper(Obra)
     return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
 
-@app.route('/cars', methods=['GET'])
-def list_cars():
-    myjson = get_objects_helper(Car)
+@app.route('/saga', methods=['GET'])
+def list_saga():
+    myjson = get_objects_helper(Saga)
     return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
 
-# --- DELETE's ---
+@app.route('/raca', methods=['GET'])
+def list_raca():
+    myjson = get_objects_helper(Raca)
+    return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
+
+@app.route('/transformacao', methods=['GET'])
+def list_transformacao():
+    myjson = get_objects_helper(Transformacao)
+    return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
+
+@app.route('/personagembase', methods=['GET'])
+def list_personagembase():
+    myjson = get_objects_helper(PersonagemBase)
+    return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
+
+@app.route('/personagemsaga', methods=['GET'])
+def list_personagemsaga():
+    myjson = get_objects_helper(PersonagemSaga)
+    return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
+
+# @app.route('/example', methods=['GET'])
+# def list_example():
+#     myjson = get_objects_helper(Anus)
+#     return jsonify(myjson), 200 if myjson['result'] == 'ok' else 500
+
+# ---------------------------------------------------------------- DELETE's ----------------------------------------------------------------
 
 def delete_object(mclass, obj_id):
     try:
@@ -80,14 +169,50 @@ def delete_object(mclass, obj_id):
         print(f"Error during hard delete of object {mclass} with id {obj_id}: {ex}")
         return {"result": "error", "details": f"error during object ({mclass}) exclusion: {ex}"}
 
-@app.route('/persons/<int:obj_id>', methods=['DELETE'])
-def delete_person(obj_id):
-    myjson = delete_object(Person, obj_id)
+# @app.route('/persons/<int:obj_id>', methods=['DELETE'])
+# def delete_person(obj_id):
+#     myjson = delete_object(Person, obj_id)
+#     return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
+
+# @app.route('/cars/<int:obj_id>', methods=['DELETE'])
+# def delete_car(obj_id):
+#     myjson = delete_object(Car, obj_id)
+#     return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
+
+@app.route('/obra/<int:obj_id>', methods=['DELETE'])
+def delete_obra(obj_id):
+    myjson = delete_object(Obra, obj_id)
     return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
 
-@app.route('/cars/<int:obj_id>', methods=['DELETE'])
-def delete_car(obj_id):
-    myjson = delete_object(Car, obj_id)
+@app.route('/saga/<int:obj_id>', methods=['DELETE'])
+def delete_saga(obj_id):
+    myjson = delete_object(Saga, obj_id)
     return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
 
+@app.route('/raca/<int:obj_id>', methods=['DELETE'])
+def delete_raca(obj_id):
+    myjson = delete_object(Raca, obj_id)
+    return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
+
+@app.route('/transformacao/<int:obj_id>', methods=['DELETE'])
+def delete_transformacao(obj_id):
+    myjson = delete_object(Transformacao, obj_id)
+    return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
+
+@app.route('/personagembase/<int:obj_id>', methods=['DELETE'])
+def delete_personagembase(obj_id):
+    myjson = delete_object(PersonagemBase, obj_id)
+    return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
+
+@app.route('/personagemsaga/<int:obj_id>', methods=['DELETE'])
+def delete_personagemsaga(obj_id):
+    myjson = delete_object(PersonagemSaga, obj_id)
+    return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
+
+# @app.route('/example/<int:obj_id>', methods=['DELETE'])
+# def delete_example(obj_id):
+#     myjson = delete_object(Example, obj_id)
+#     return jsonify(myjson), 204 if myjson['result'] == 'ok' else 500
+
+# ---------------------------------------------------------------- END ----------------------------------------------------------------
 print("Routes loaded successfully. (reached routes.py)")
--- README.md
+++ README.md
@@ -7,4 +7,11 @@ Saga: (Nome, desc, ep_ini, ep_fin, Obra_id, imagem)
 Raça: (Nome, cor, desc, poderBase, imagem)
 Transformação: (Nome, cor, poder, especial, efeitoCol, imagem, tempo_lim)
 PersonagemBase: (Nome, Raça_id, data_nasc, sexo,  imagem)
+
 PersonagemSaga: (PersonagemBase_id, Saga_id, poder, Trans_id[], imagem)
+
+1) baixar .zip em node.js
+descompactar em c:\node
+2) abrir powershell no modo admin
+3) executar: 
+ setx /M PATH "%PATH%;C:\node\"  
